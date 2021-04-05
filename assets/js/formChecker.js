c7b62746253c3a50b7af19c76291f2486fa06234
Konstantin <konstantinstepanyan99@gmail.com>
2021-04-05 17:33:58 +0300

    remove btn

--- assets/js/formChecker.js
+++ assets/js/formChecker.js
@@ -1,17 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const phoneInput = document.querySelector('.getPresentation__text_phone');
-    const regV = /^([+]?[0-9\s-\(\)]{3,25})*$/i;
+    const btns = document.querySelectorAll('.social-network__a');
+
+
+    btns.forEach((item, index) => {
+        item.addEventListener('click', (e) => {
+
+            checkForm(e, item);
+        })
+    });
+
 
     phoneInput.addEventListener('blur', (e) => {
-        const phoneInputValue = phoneInput.value.trim();
-
-        console.log(phoneInputValue)
-
-        if (regV.test(phoneInputValue)) {
-            phoneInput.style.border = '1px solid grey';
-        } else {
-            phoneInput.style.border = '2px solid red';
-        }
+        checkForm(e);
 
     });
+
+
+
+
+
+    function checkForm(e, item) {
+        const newPhoneInput = document.querySelector('.getPresentation__text_phone');
+        const regV = /^([+]?[0-9\s-\(\)]{3,25})*$/i;
+        const phoneInputValue = newPhoneInput.value.trim();
+
+
+
+        if (e.type == 'blur') {
+            if (regV.test(phoneInputValue)) {
+//                console.log('blur');
+//                console.log(regV.test(phoneInputValue))
+                phoneInput.style.border = '1px solid grey';
+            } else {
+//                console.log('blur');
+//                console.log(regV.test(phoneInputValue))
+                phoneInput.style.border = '2px solid red';
+
+            }
+        }
+        if (e.type == 'click') {
+                
+            e.preventDefault();
+
+            
+            if (phoneInputValue != '') {
+
+                phoneInput.style.border = '2px solid grey';
+            } else {
+
+                phoneInput.style.border = '2px solid red';
+            }
+        }
+
+
+    }
 });
--- prod/assets/js/script.js
+++
@@ -1,3 +0,0 @@
-jQuery(function($){
-	$('.getPresentation__text_phone').mask("+7 (999) 999-99-99");
-});
